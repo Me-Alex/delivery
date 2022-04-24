e2e06a60f4ec6a7038e2cffeb1ffda4de541179a
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2022-04-25 02:20:38 +0300

    final-early-acces-design

--- js/main.js
+++ js/main.js
@@ -1,17 +1,17 @@
-let btn1 = document.querySelector("#button-yes");
-let btn2 = document.querySelector("#button-no");
 let signIn = document.querySelector(".sign-in");
 let buttons = document.querySelector(".question-buttons");
 let text = document.querySelector(".h1-text");
-let closeWindow = document.querySelector(".close-window");
-let container = document.querySelector(".index-container-header");
-let accountExist = document.querySelector(".user-has-an-account");
+let container = document.querySelector(".index-container-header-none");
 let loginForm = document.querySelector(".log-in");
+let menuline = document.querySelectorAll(".menu-line");
+let logInShow = document.querySelector(".log-in-show");
 
 
 
 
 
+
+let btn1 = document.querySelector("#button-yes");
 btn1.onclick = () => {
     console.log("da");
     signIn.style.display = "inherit";
@@ -19,15 +19,40 @@ btn1.onclick = () => {
     text.innerText = "You need to sign in first";
     console.log("ceve");
 };
+
+
+let btn2 = document.querySelector("#button-no");
 btn2.onclick = () => {
     console.log("nu");
     text.innerText = "Te doare la pula";
 }
-closeWindow.onclick = () => {
-    container.style.display = "none";
-}
+
+// let closeWindow = document.querySelector(".close-window");
+// closeWindow.onclick = () => {
+//     container.style.display = "none";
+// }
+
+let accountExist = document.querySelector(".user-has-an-account");
 accountExist.onclick = () => {
-    loginForm.style.display="inherit";
+    loginForm.style.display = "inherit";
     signIn.style.display = "none";
     text.innerText = "Log in";
+}
+
+
+let menu = document.querySelector(".menu-lines");
+menu.onclick = () => {
+    menu.classList.toggle("menu-lines1");
+    // container.style.display = "inherit";
+    menu.style.zIndex = "1";
+    childs = menu.children;
+    for (let i = 0; i < childs.length; i++) {
+        childs[i].classList.toggle("menu-line" + (i + 1));
+    }
+    container.classList.toggle("index-container-header-normal");
+}
+let menu1 = document.querySelector(".menu-new");
+
+logInShow.onclick = () => {
+    container.style.display = "inherit";
 }
