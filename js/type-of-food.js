103a3c89c6e23100fb65f0afa743558f881aa744
Me-Alex <60211801+Me-Alex@users.noreply.github.com>
2022-04-28 00:11:07 +0300

    added test feature more details page for retaurant

--- js/type-of-food.js
+++ js/type-of-food.js
@@ -1,15 +1,23 @@
-let typeOfFood=document.querySelector(".type-of-food");
+let typeOfFood = document.querySelector(".type-of-food");
 for (let i = 0; i < typeOfFood.children.length; i++) {
 
     typeOfFood.children[i].onclick = () => {
-        let store=i;
+        let store = i;
         starReview = i;
         for (let i = 0; i < typeOfFood.children.length; i++) {
             typeOfFood.children[i].classList = "";
         }
-            typeOfFood.children[i].classList.toggle("type-of-food-clicked");
-        console.log( typeOfFood.children[i].innerHTML);
+        typeOfFood.children[i].classList.toggle("type-of-food-clicked");
+        console.log(typeOfFood.children[i].innerHTML);
 
         // raiting.children[i].classList.toggle("checked");
     }
 }
+
+
+//what happends if you click on a restaurant listed on the page
+let restaurant = document.querySelectorAll(".restaurant-result");
+for (let i = 0; i < restaurant.length;i++)
+    restaurant[i].onclick = () => {
+        window.location.href = "detalied-info-restaurant.html";
+    }
